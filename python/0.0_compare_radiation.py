c52a8f4d8cf9bfe14de5fd95e581d924dd9a19bd
cdoedens <cdoedens@gmail.com>
2025-05-14 10:23:32 +1000

    icon-ceres diffs

--- python/0.0_compare_radiation.py
+++ python/0.0_compare_radiation.py
@@ -49,7 +49,7 @@ era5_ceres_diff = era5_out.mean(dim='time') - ceres.toa_sw_all_mon.mean(dim='tim
 era5_ceres_rmse = np.sqrt(np.square(era5_ceres_diff).weighted(np.cos(np.deg2rad(era5_ceres_diff.lat))).mean()).values
 era5_ceres_rmse
 
-# UK node model data
+##### UK node model data #####
 
 um = xr.open_zarr('/g/data/qx55/uk_node/glm.n2560_RAL3p3/data.healpix.PT1H.z5.zarr')
 um_data = um.rsut.sel(time=slice('2020-03', '2021-02')).mean(dim='time') *-1
@@ -61,8 +61,8 @@ data = um_data
 axes.set_global()
 im = egh.healpix_show(data.values,ax=axes)
 axes.coastlines()
-axes.set_title(f'UK N2560 RAL3 healpix')
-plt.savefig('figures/healpix-test.png')
+axes.set_title(f'UK healpix')
+plt.savefig('figures/healpix-test-um.png')
 
 # Regrid healpix to era5
 
@@ -85,16 +85,49 @@ lon = ceres['lon'].values
 lat = ceres['lat'].values
 
 # nside for um simulation, it should be equal to 2**zoom
-this_nside = hp.get_nside(um.rsut.mean(dim='time'))
+this_nside = hp.get_nside(um_data)
 
 cells = get_nn_lon_lat_index(this_nside, lon, lat) 
 
 # Calculate UM-CERES difference
 
-
 um_ceres_diff = um_data.isel(cell = cells) - ceres.toa_sw_all_mon.mean(dim='time')
-um.rsut
-# Plotting
+um_ceres_rmse = np.sqrt(np.square(um_ceres_diff).weighted(np.cos(np.deg2rad(um_ceres_diff.lat))).mean()).values
+
+##### GER node model data #####
+
+icon = xr.open_zarr('/g/data/qx55/germany_node/d3hp003.zarr/P1D_mean_z5_atm.zarr')
+icon_data = icon.rsut.sel(time=slice('2020-03', '2021-02')).mean(dim='time') *-1
+
+projection = ccrs.Robinson(central_longitude=0)
+
+fig, axes = plt.subplots(figsize=(10, 6), subplot_kw={'projection': projection}, layout='constrained')
+data = icon_data
+axes.set_global()
+im = egh.healpix_show(data.values,ax=axes)
+axes.coastlines()
+axes.set_title(f'ICON healpix')
+plt.savefig('figures/healpix-test-icon.png')
+
+# Regrid healpix to era5
+# Find the HEALPix pixels that are closest to the ERA5 grid
+
+# longitudes and latitudes for the ERA5 grid
+lon = ceres['lon'].values
+lat = ceres['lat'].values
+
+# nside for um simulation, it should be equal to 2**zoom
+this_nside = hp.get_nside(icon_data)
+
+cells = get_nn_lon_lat_index(this_nside, lon, lat)
+
+# Calculate ICON-CERES difference
+
+icon_ceres_diff = icon_data.isel(cell = cells) - ceres.toa_sw_all_mon.mean(dim='time')
+icon_ceres_rmse = np.sqrt(np.square(icon_ceres_diff).weighted(np.cos(np.deg2rad(icon_ceres_diff.lat))).mean()).values
+
+
+##### Plotting #####
 nrow = 1
 ncol = 4
 fm_bottom = 1.5 / (4.4*nrow + 2)
@@ -103,7 +136,12 @@ fig, axs = plt.subplots(
     subplot_kw={'projection': ccrs.Mollweide(central_longitude=180)},
     gridspec_kw={'hspace': 0.01, 'wspace': 0.01},)
 
-plt_colnames = ['CERES', 'ERA5', 'ERA5-CERES', 'UM-CERES']
+plt_colnames = [
+    'CERES',
+    f'ERA5-CERES, RMSE: {era5_ceres_rmse:.2f}',
+    f'UM-CERES, RMSE: {um_ceres_rmse:.2f}',
+    f'ICON-CERES, RMSE: {icon_ceres_rmse:.2f}',
+    ]
 for jcol in range(ncol):
     axs[jcol] = globe_plot(ax_org=axs[jcol])
     axs[jcol].text(
@@ -114,7 +152,7 @@ for jcol in range(ncol):
 pltlevel, pltticks, pltnorm, pltcmp = plt_mesh_pars(
     cm_min=-250, cm_max=-30, cm_interval1=10, cm_interval2=20, cmap='viridis',)
 pltlevel2, pltticks2, pltnorm2, pltcmp2 = plt_mesh_pars(
-    cm_min=-40, cm_max=40, cm_interval1=5, cm_interval2=10, cmap='BrBG')
+    cm_min=-80, cm_max=80, cm_interval1=10, cm_interval2=20, cmap='BrBG')
 
 axs[0].pcolormesh(
         ceres.lon, ceres.lat,
@@ -122,18 +160,18 @@ axs[0].pcolormesh(
         norm=pltnorm, cmap=pltcmp, transform=ccrs.PlateCarree())
 
 axs[1].pcolormesh(
-        era5_out.lon, era5_out.lat,
-        era5_out.mean(dim='time'),
-        norm=pltnorm, cmap=pltcmp, transform=ccrs.PlateCarree())
-
-axs[2].pcolormesh(
         era5_ceres_diff.lon, era5_ceres_diff.lat,
         era5_ceres_diff,
         norm=pltnorm2, cmap=pltcmp2, transform=ccrs.PlateCarree())
 
-axs[3].pcolormesh(
+axs[2].pcolormesh(
         um_ceres_diff.lon, um_ceres_diff.lat,
         um_ceres_diff,
+        norm=pltnorm2, cmap=pltcmp2, transform=ccrs.PlateCarree())
+
+axs[3].pcolormesh(
+        icon_ceres_diff.lon, icon_ceres_diff.lat,
+        icon_ceres_diff,
         norm=pltnorm2, cmap=pltcmp2, transform=ccrs.PlateCarree())
 
 cbar = fig.colorbar(
@@ -150,4 +188,4 @@ cbar2 = fig.colorbar(
 cbar2.ax.set_xlabel('Colorbar label 2')
 
 fig.subplots_adjust(left=0.001, right=0.999, bottom=fm_bottom, top=0.94)
-fig.savefig('figures/test.png')
+fig.savefig('figures/ceres_diffs.png')
